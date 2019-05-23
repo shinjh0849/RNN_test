6f3118c10f23ba950f5b80e99d2afa35f42f0d1a
신지호 <jihoshin@sinjihoui-MacBookPro.local>
2019-05-24 06:42:07 +0900

    final

--- rnn.py
+++ rnn.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import numpy as np
+from numpy import concatenate
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import Imputer
@@ -9,9 +10,32 @@ from pandas import concat
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
+# def f1_score(y_true, y_pred):
+# 	y_pred = np.round(y_pred)
+# 	print(y_true.shape)
+# 	print(y_pred.shape)
+# 	print(y_true)
+# 	print(y_pred)
+
+# 	TP = tf.count_nonzero(y_true * y_pred)
+# 	TN = tf.count_nonzero((y_true-1) * (y_pred-1))
+# 	FP = tf.count_nonzero((y_true-1) * y_pred)
+# 	FN = tf.count_nonzero(y_true * (y_pred-1))
+
+# 	precision = TP / (TP + FP)
+# 	recall = TP / (TP + FN)
+# 	f1 = 2 * precision * recall / (precision + recall)
+# 	return f1
 
 # convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
@@ -62,35 +86,25 @@ train_set.to_csv('TrainsetTransformed.csv')
 
 # saving the transformed file
 dataset = pd.read_csv('TrainsetTransformed.csv', header=0, index_col=0)
-testset = pd.read_csv('TestsetTransformed.csv', engine='python', skipinitialspace=True, skiprows=1, names=FEATURES)
+testset = pd.read_csv('TestsetTransformed.csv', header=0, index_col=0)
 
+# making both train set & test set csv to sequential data
 values_train = dataset.values
-encoder = LabelEncoder()
+encoder_train = LabelEncoder()
 values_train = values_train.astype('float32')
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_train = scaler.fit_transform(values_train)
+scaler_train = MinMaxScaler(feature_range=(0,1))
+scaled_train = scaler_train.fit_transform(values_train)
 reframed_train = series_to_supervised(scaled_train, 1, 1)
+reframed_train.drop(reframed_train.columns[[1]], axis=1, inplace=True)
 
-# init
-# values_test = testset.values
-# values_test = values_test.astype('float32')
-# scaled_test = scaler.fit_transform(values_test)
-# reframed_test = series_to_supervised(scaled_test, 1, 1)
-
-# # defining the model
-# values_train = reframed_train.values
-# values_test = reframed_test.values
-# train = values_train[:, :]
-# test = values_test[:, :]
-# train_X, train_Y = train[:, :-1], train[:, -1]
-# test_X, test_Y = test[:, :-1], test[:, -1]
-# train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-# test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-# print(train_X.shape, train_Y.shape, test_X.shape, test_Y.shape)
+values_test = testset.values
+encoder_test = LabelEncoder()
+values_test = values_test.astype('float32')
+scaler_test = MinMaxScaler(feature_range=(0,1))
+scaled_test = scaler_test.fit_transform(values_test)
+reframed_test = series_to_supervised(scaled_test, 1, 1)
 
 
-
-# second try
 values = reframed_train.values
 train = values[:, :]
 validation = values[:, :]
@@ -102,22 +116,49 @@ train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 validation_X = validation_X.reshape((validation_X.shape[0], 1, validation_X.shape[1]))
 print(train_X.shape, train_Y.shape, validation_X.shape, validation_Y.shape)
 
+values_test = reframed_test.values
+test = values_test[:, :]
+test_X = test[:, :]
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 
+
+# using SMOTE to resolve class imbalance problem
+sm = SMOTE(ratio='auto', kind='regular')
+ros = RandomOverSampler(random_state=0)
+resam_X = train_X.reshape((train_X.shape[0], train_X.shape[2]))
+
+# X_resampled, y_resampled = ros.fit_resample(resam_X, train_Y)
+X_resampled, Y_resampled = sm.fit_resample(resam_X, train_Y)
+
+X_resampled = X_resampled.reshape((X_resampled.shape[0], 1, X_resampled.shape[1]))
+print(X_resampled.shape)
 
 # design the model
 model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(LSTM(50, input_shape=(X_resampled.shape[1], X_resampled.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adagrad')
 
-history = model.fit(train_X, train_Y, epochs=500, batch_size=72, validation_data=(validation_X, validation_Y), verbose=2, shuffle=False)
+history = model.fit(X_resampled, Y_resampled, epochs=200, batch_size=72, 
+					validation_data=(validation_X, validation_Y), verbose=2, shuffle=False)
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='valdation')
 plt.legend()
 plt.show()
 
 # evaluating model
-#yhat = model.predict(test_X)
+yhat_valid = model.predict(validation_X)
+yhat_valid = np.round(yhat_valid)
+DataFrame(yhat_valid).to_csv('validation.csv')
+
+print('f1_score: ', f1_score(validation_Y, yhat_valid))
+print('accuracy: ', accuracy_score(validation_Y, yhat_valid))
+
+yhat_test = model.predict(test_X)
+yhat_test = np.round(yhat_test)
+
+DataFrame(yhat_test).to_csv('prediction.csv')
+
 
 # plotting the dataset
 # groups = [0, 1, 2, 3, 4, 5, 6]
@@ -129,7 +170,3 @@ plt.show()
 #     plt.title(dataset.columns[group], y=0.5, loc='right')
 #     i += 1
 # plt.show()
-
-# convert series to supervised learning
-# https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
-
